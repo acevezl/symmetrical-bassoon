f93b1c8cfce3369e793bc4b2310838283d1a8435
Arnaut <lou.arnaut@gmail.com>
2021-07-27 16:16:44 -0700

    completed offline transaction save functionality

--- public/js/index.js
+++ public/js/index.js
@@ -136,7 +136,7 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
-    saveRecord(transaction);
+    saveTrx(transaction);
 
     // clear form
     nameEl.value = "";
